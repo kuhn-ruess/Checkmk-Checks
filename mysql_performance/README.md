7073bf3e1111e17337460ffc63eaf3afcba2c382
Bastian Kuhn <mail@bastian-kuhn.de>
2018-09-27 12:57:46 +0200

    Added subject to monitor MySQL Thread Cache Hitrate

---
+++ mysql_performance/README.md
@@ -0,0 +1,9 @@
+# Mysql Performance
+
+This Check is a simple subcheck for the existing Check_MK Mysql Monitoring. That means, in order for this checks to work,
+the mysql plugin has to be setup.
+
+All Parameters can be configurted in WATO.
+
+## Function:
+ - Thread Cache Hitrate
---
+++ mysql_performance/src/web/plugins/wato/mysql.py
@@ -0,0 +1,40 @@
+# +-----------------------------------------------------------------+
+# |                                                                 |
+# |        (  ___ \     | \    /\|\     /||\     /|( (    /|        |
+# |        | (   ) )    |  \  / /| )   ( || )   ( ||  \  ( |        |
+# |        | (__/ /     |  (_/ / | |   | || (___) ||   \ | |        |
+# |        |  __ (      |   _ (  | |   | ||  ___  || (\ \) |        |
+# |        | (  \ \     |  ( \ \ | |   | || (   ) || | \   |        |
+# |        | )___) )_   |  /  \ \| (___) || )   ( || )  \  |        |
+# |        |/ \___/(_)  |_/    \/(_______)|/     \||/    )_)        |
+# |                                                                 |
+# | Copyright Bastian Kuhn 2018                mail@bastian-kuhn.de |
+# +-----------------------------------------------------------------+
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+
+register_check_parameters(
+    subgroup_applications,
+    "mysql_tchitrate",
+    _("Mysql Thread Cache Hitrate"),
+    Levels(
+        title = _("Check Levels"),
+        unit = _("Percent"),
+        default_difference = (5, 8),
+        default_value = None,
+    ),
+    TextAscii(
+        title = _("MySQL Instance"),
+        allow_empty = True
+    ),
+    'first'
+)
