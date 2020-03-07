f1292a0052bd684b6f29ba31a1d413ab30ba7265
Bastian Kuhn <mail@bastian-kuhn.de>
2020-03-07 14:35:51 +0100

    Added PostgreSQL 10 Support #12

--- postgres/src/agents/plugins/postgres_replication.sh
+++ postgres/src/agents/plugins/postgres_replication.sh
@@ -1,2 +1,14 @@
+if su - postgres -c 'psql -V' | grep '(PostgreSQL) 10'; then
+    DIFF=pg_wal_lsn_diff
+    LAST_LOC=pg_last_wal_receive_lsn
+    CURRENT_LOC=pg_current_wal_lsn
+fi
+
+if su - postgres -c 'psql -V' | grep '(PostgreSQL) 9'; then
+    DIFF=pg_xlog_location_diff
+    LAST_LOC=pg_last_xlog_receive_location
+    CURRENT_LOC=pg_current_xlog_location
+fi
+
 echo "<<<postgres_replication>>>"
-echo "WITH slots AS (SELECT slot_name, slot_type, coalesce(restart_lsn, '0/0'::pg_lsn) AS slot_lsn, coalesce(pg_xlog_location_diff(coalesce(pg_last_xlog_receive_location(), pg_current_xlog_location()), restart_lsn),0) AS delta, active FROM pg_replication_slots) SELECT *, pg_size_pretty(delta) AS delta_pretty FROM slots; " | su - postgres -c "psql -d postgres -A -t -F' '"
+echo "WITH slots AS (SELECT slot_name, slot_type, coalesce(restart_lsn, '0/0'::pg_lsn) AS slot_lsn, coalesce(${DIFF}(coalesce(${LAST_LOC}(), ${CURRENT_LOC}()), restart_lsn),0) AS delta, active FROM pg_replication_slots) SELECT *, pg_size_pretty(delta) AS delta_pretty FROM slots; " | su - postgres -c "psql -d postgres -A -t -F' '"
