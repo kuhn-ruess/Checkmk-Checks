3103d511f2b1d058594984140ca11337bf052ba6
Bastian Kuhn <mail@bastian-kuhn.de>
2020-04-16 09:17:30 +0200

    Fixed internal namings

--- open_iscsi/src/web/plugins/wato/open-iscsi.py
+++ open_iscsi/src/web/plugins/wato/open-iscsi.py
@@ -1,7 +1,7 @@
 
 group = "agents/" + _("Agent Plugins")
 register_rule(group,
-    "agent_config:open_isci",
+    "agent_config:open_iscsi",
     DropdownChoice(
         title = _("Deploy Open-iSCSI  Plugin (Linux)"),
         help = _("The plugin allows monitoring of Open iSCSI Devices"),
