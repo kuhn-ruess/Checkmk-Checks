0b66c4f8856b6df02cd79f13f9e85025b5cabb93
Konstantin Büttner <konstantin.buettner@gmail.com>
2022-07-18 18:15:07 +0200

    catalyst_switch_state: Check to monitor state and role of Catalyst switches

---
+++ catalyst_switch_state/src/web/plugins/wato/catalyst_switch_state.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    DropdownChoice,
+)
+
+from cmk.gui.plugins.wato import (
+    CheckParameterRulespecWithItem,
+    rulespec_registry,
+    RulespecGroupCheckParametersNetworking,
+)
+
+
+def _parameter_valuespec_catalyst_switch_state():
+    return Dictionary(
+            elements=[
+                ("switch_role",
+                    DropdownChoice(
+                        title=_("Expected switch role"),
+                        choices=[
+                            ("1", "master"),
+                            ("2", "member"),
+                            ("3", "not member"),
+                            ("4", "standby"),
+                        ],
+                    )
+                ),
+            ],
+        )
+
+rulespec_registry.register(
+    CheckParameterRulespecWithItem(
+        check_group_name="catalyst_switch_state",
+        group=RulespecGroupCheckParametersNetworking,
+        item_spec=lambda: TextAscii(title=_("Switch number")),
+        match_type="dict",
+        parameter_valuespec=_parameter_valuespec_catalyst_switch_state,
+        title=lambda: _("Catalyst Switch State"),
+    )
+)
