9762fbc9800eb649beb3ac30637bb8ce1b552307
Bastian Kuhn <mail@bastian-kuhn.de>
2023-07-21 11:58:42 +0200

    quobyte: Added levels for volumes

--- quobyte/src/agent_based/quobyte_volumes.py
+++ quobyte/src/agent_based/quobyte_volumes.py
@@ -32,29 +32,53 @@ def discover_quobyte_volumes(section):
     for volume in section:
         yield Service(item=volume)
 
+def check_levels(current, levels):
+    """
+    Helper
+    """
+    warn, crit = levels
+    if current >= crit:
+        return State.CRIT
+    elif current >= warn:
+        return State.WARN
+    return State.OK
+
+
 def check_quobyte_volumes(item, params, section):
     """
     Check single Volume
     """
     data = section[item]
     state = State.OK
+
     if 'used_allocated_space_bytes' in data:
+        if levels := params.get('used_allocated_space_bytes'):
+            state = check_levels(data['used_allocated_space_bytes'], levels)
+
         yield Result(state=state, summary=f"Disk Used {render.bytes(data['used_allocated_space_bytes'])}")
         yield Metric("disk_used", data['used_allocated_space_bytes'])
 
     if 'file_count' in data:
+        if levels := params.get('file_count'):
+            state = check_levels(data['file_count'], levels)
         yield Result(state=state, summary=f"Files Used {data['file_count']}")
         yield Metric("files_total", data['file_count'])
 
     if 'directory_count' in data:
+        if levels := params.get('directory_count'):
+            state = check_levels(data['directory_count'], levels)
         yield Result(state=state, summary=f"Directories Used {data['directory_count']}")
         yield Metric("directories_total", data['directory_count'])
 
     if 'used_logical_space_bytes' in data:
+        if levels := params.get('used_logical_space_bytes'):
+            state = check_levels(data['used_logical_space_bytes'], levels)
         yield Result(state=state, summary=f"Logical Used {render.bytes(data['used_logical_space_bytes'])}")
         yield Metric("logical_used", data['used_logical_space_bytes'])
 
     if 'used_disk_space_bytes' in data:
+        if levels := params.get('used_disk_space_bytes'):
+            state = check_levels(data['used_disk_space_bytes'], levels)
         yield Result(state=state, summary=f"Physical Used {render.bytes(data['used_disk_space_bytes'])}")
         yield Metric("physical_used", data['used_disk_space_bytes'])
 
@@ -70,4 +94,5 @@ register.check_plugin(
     discovery_function=discover_quobyte_volumes,
     check_function=check_quobyte_volumes,
     check_default_parameters={},
+    check_ruleset_name="quobyte_volumes",
 )
--- quobyte/src/web/plugins/wato/quobyte.py
+++ quobyte/src/web/plugins/wato/quobyte.py
@@ -1,10 +1,10 @@
-# 2021 created by Sven Rueß, sritd.de
-
 from cmk.gui.i18n import _
 from cmk.gui.valuespec import (
     Dictionary,
     Password,
     TextInput,
+    Integer,
+    Filesize,
 )
 from cmk.gui.plugins.wato import (
     rulespec_registry,
@@ -12,6 +12,15 @@ from cmk.gui.plugins.wato import (
 )
 from cmk.gui.plugins.wato.datasource_programs import (
     RulespecGroupDatasourceProgramsHardware,
+)
+
+from cmk.gui.watolib.rulespec_groups import (
+    RulespecGroupEnforcedServicesStorage,
+)
+
+from cmk.gui.plugins.wato.utils import (
+    CheckParameterRulespecWithItem,
+    CheckParameterRulespecWithoutItem,
 )
 
 
@@ -36,3 +45,40 @@ rulespec_registry.register(
     )
 )
 
+def _parameter_valuespec_quobyte_volumes():
+    return Dictionary(
+        title=_("Quobyte Volume Levels"),
+        elements=[
+            (
+                "used_allocated_space_bytes",
+                SimpleLevels(Filesize, title=_("Allocated Space"), default_levels=(65.0, 90.0)),
+            ),
+            (
+                "used_logical_space_bytes",
+                SimpleLevels(Filesize, title=_("Used Logical Space "), default_levels=(65.0, 90.0)),
+            ),
+            (
+                "used_disk_space_bytes",
+                SimpleLevels(Filesize, title=_("Used Disk Space"), default_levels=(65.0, 90.0)),
+            ),
+            (
+                "file_count",
+                SimpleLevels(Integer, title=_("Total Count of Files"), default_levels=(0, 0)),
+            ),
+            (
+                "directory_count",
+                SimpleLevels(Integer, title=_("Total Count of Directories"), default_levels=(0, 0)),
+            ),
+        ],
+    )
+
+rulespec_registry.register(
+    CheckParameterRulespecWithItem(
+        check_group_name="quobyte_volumes",
+        group=RulespecGroupEnforcedServicesStorage,
+        match_type="dict",
+        item_spec=lambda: TextInput(title=_("Volume Name")),
+        parameter_valuespec=_parameter_valuespec_quobyte_volumes,
+        title=lambda: _("Quobyte Volumes"),
+    )
+)
