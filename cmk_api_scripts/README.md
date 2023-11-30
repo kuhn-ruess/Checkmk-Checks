4111ac43f35b72ac1b729be67b6acfd2a6945218
Bastian Kuhn <mail@bastian-kuhn.de>
2023-11-30 11:23:57 +0100

    Added Activate Changes Script

---
+++ cmk_api_scripts/README.md
@@ -0,0 +1,13 @@
+# CMK API Script
+
+
+## activate_changes.py
+Activate changes with the CMK2.x API. Please set Url, User and Password inside the Script
+
+Note: If the Activation fails, the Checkmk API as least of 2.2.0p14 still returns an OK state.
+The Script therfore can't detect that.
+### Features:
+- Detect open Changes
+- Trigger Activate Changes
+- Waits for Activate to be finsh
+- Exits if changes are made while the script is started.
---
+++ cmk_api_scripts/activate_changes.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+"""
+Simple Script to trigger Activate Changes e.g. by Cronjob
+
+Bastian Kuhn, bastian.kuhn@kuhn-ruess.de
+"""
+import sys
+import requests
+
+SERVER_ADDRESS = "http://192.168.178.22:5002/cmk"
+USERNAME = "cmkadmin"
+PASSWORD = "Test123$"
+VERIFY_SSL = False
+TIMEOUT=15
+
+DEBUG = False
+
+
+def print_debug(what):
+    """
+    Print String if Debug is set
+    """
+    if DEBUG:
+        print(what)
+
+try:
+    print("Started Activate Changes")
+    base_url = f'{SERVER_ADDRESS}/check_mk/api/1.0/'
+    base_headers = {
+        'Authorization': f"Bearer {USERNAME} {PASSWORD}"
+    }
+
+    # Get current activation etag
+    print(" - Get current Pending Changes")
+    url = base_url +  "/domain-types/activation_run/collections/pending_changes"
+    response = requests.get(url, headers=base_headers, verify=VERIFY_SSL, timeout=TIMEOUT)
+    print_debug(response.json())
+    etag = response.headers.get('ETag')
+    if not response.json()['value']:
+        print(" - Nothing todo, end")
+        sys.exit(0)
+
+    activate_headers = {
+        'if-match': etag
+    }
+    activate_headers.update(base_headers)
+
+    # Trigger Activate Changes
+    print(" - Trigger Activate Changes")
+    url = base_url + "/domain-types/activation_run/actions/activate-changes/invoke"
+    json = {
+        'redirect': False,
+        'force_foreign_changes': True,
+    }
+    response = requests.post(url, json=json, headers=activate_headers,
+                             verify=VERIFY_SSL, timeout=TIMEOUT)
+
+    print_debug(response.json())
+    response_json = response.json()
+    if 'id' not in response_json:
+        print("Cant activate Changes currently, try again")
+        sys.exit(1)
+
+    activation_id = response_json['id']
+
+
+    # Wait and Check for Status
+    print(" - Wait for Activation")
+    url = \
+        f"{base_url}/objects/activation_run/{activation_id}/actions/wait-for-completion/invoke"
+    response = requests.get(url, headers=base_headers, verify=VERIFY_SSL, timeout=TIMEOUT)
+    if response.status_code != 204:
+        print("Activation not possible, see details below")
+        print(response.json())
+        sys.exit(1)
+
+    print(" -- Done")
+
+    print_debug(f"Activation Response: {response.status_code}")
+    print("Ended Activate Changes")
+except Exception as error: #pylint: disable=broad-exception-caught
+    print(f"Error: {error}")
+    if DEBUG:
+        raise
