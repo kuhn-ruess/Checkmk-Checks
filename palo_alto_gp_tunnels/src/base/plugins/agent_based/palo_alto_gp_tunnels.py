e1a594cf5e8e3a25c644e87b677889aa81955c96
Sven Rueß <github@sven-ruess.de>
2024-02-06 03:28:42 +0100

    Added Palo Alto GP Tunnels check
    Signed-off-by: Sven Rueß <github@sven-ruess.de>

---
+++ palo_alto_gp_tunnels/src/base/plugins/agent_based/palo_alto_gp_tunnels.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from typing import NamedTuple, Optional
+from .agent_based_api.v1.type_defs import (
+    CheckResult,
+    DiscoveryResult,
+    StringTable,
+)
+
+from .agent_based_api.v1 import (
+    all_of,
+    startswith,
+    exists,
+    Metric,
+    register,
+    Result,
+    Service,
+    SNMPTree,
+    State,
+)
+
+
+class PaloAtoTunnels(NamedTuple):
+    active_tunnels: int
+    max_tunnels: int
+
+def parse_palo_alto_tunnels(string_table: StringTable) -> Optional[PaloAtoTunnels]:
+    if not string_table:
+        return None
+    snmp_data = [int(s) for s in string_table[0]]
+    return PaloAtoTunnels(*snmp_data)
+
+
+
+register.snmp_section(
+    name="palo_alto_gp_tunnels",
+    parse_function=parse_palo_alto_tunnels,
+    fetch=SNMPTree(
+        base=".1.3.6.1.4.1.25461.2.1.2.5.1",
+        oids=["3", "2"],
+    ),
+    detect=all_of(
+        startswith(".1.3.6.1.2.1.1.1.0", 'Palo Alto'),
+        exists(".1.3.6.1.4.1.25461.2.1.2.5.1.*"),
+    ),
+)
+
+
+def discover_palo_alto_tunnels(section: PaloAtoTunnels) -> DiscoveryResult:
+    yield Service()
+
+
+def check_palo_alto_tunnels(section: PaloAtoTunnels) -> CheckResult:
+    yield Metric('active_gp_tunnels',
+                 section.active_tunnels,
+                 boundaries=(0, section.max_tunnels))
+
+    text = f"Currently {section.active_tunnels} of {section.max_tunnels} Possible GlobalProtect Tunnels used"
+
+    if section.active_tunnels >= section.max_tunnels - 15:
+        yield Result(state=state.CRIT, summary=text)
+
+    elif section.active_tunnels >= section.mac_tunnels - 50:
+        yield Result(state=state.WARN, summary=text)
+
+    else:
+        yield Result(state=state.OK, summary=text)
+
+
+register.check_plugin(
+    name="palo_alto_gp_tunnels",
+    service_name="Palo Alto GlobalProtect Tunnels",
+    discovery_function=discover_palo_alto_tunnels,
+    check_function=check_palo_alto_tunnels,
+)
