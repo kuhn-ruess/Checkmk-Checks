8782fd3ec8637b41f1110b6d878545124fbafd1e
Moritz Schlarb <schlarbm@uni-mainz.de>
2024-12-04 18:06:04 +0100

    Update mysql_status.py (#87)
    Closes: #76

--- mysql_status/src/web/plugins/perfometer/mysql_status.py
+++ mysql_status/src/web/plugins/perfometer/mysql_status.py
@@ -45,6 +45,10 @@ except:
 
 def perfometer_mysql_status(row, check_command, perf_data):
     color = { 0: "#a4f", 1: "#ff2", 2: "#f22", 3: "#fa2" }[row["service_state"]]
-    return "%d" % int(perf_data[0][1]), perfometer_logarithmic(perf_data[0][1], 400, 2, color)
+    try:
+        value = perf_data[0].value
+    except AttributeError:
+        value = perf_data[0][1]
+    return "%d" % int(value), perfometer_logarithmic(value, 400, 2, color)
 
 perfometers['check_mk-mysql_status'] = perfometer_mysql_status
