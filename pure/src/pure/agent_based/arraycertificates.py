a8e50a7c31b5d438f39a11766e816b42047b8924
Sven Rueß <github@sven-ruess.de>
2025-05-16 00:18:26 +0200

    Pure: Added missing checks
    Signed-off-by: Sven Rueß <github@sven-ruess.de>

--- pure/src/pure/agent_based/arraycertificates.py
+++ pure/src/pure/agent_based/arraycertificates.py
@@ -1,12 +1,20 @@
-#2023 created by steve.parker@8x8.com
-#/omd/sites/xxx/local/lib/python3/cmk/base/plugins/agent_based/
-from .agent_based_api.v1 import (
-    register,
-    Service,
+#!/usr/bin/env python3
+
+"""
+Kuhn & Rueß GmbH
+Consulting and Development
+https://kuhn-ruess.de
+"""
+
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
     Result,
+    Service,
     State,
 )
-import datetime
+
+from datetime import datetime
 
 # example output lines
 #<<<pure_arraycertificates>>>
@@ -16,12 +24,14 @@ import datetime
 
 def parse_pure_certificates(string_table):
     section = {}
+
     for row in string_table:
         (name, cn, status, valid_from, valid_to, org)  = row
 
         try:
             tosecs = int(valid_to)/1000
-            valid_dt = datetime.datetime.fromtimestamp(tosecs) - datetime.datetime.now()
+            valid_dt = datetime.fromtimestamp(tosecs) - datetime.now()
+
         except ValueError:
             valid_dt = 0
 
@@ -32,16 +42,17 @@ def parse_pure_certificates(string_table):
             'valid': valid_dt.days,
             'org': org,
         }
+
     return section
 
 
-register.agent_section(
+agent_section_pure_arraycertificates = AgentSection(
     name="pure_arraycertificates",
     parse_function=parse_pure_certificates,
 )
 
 
-def discovery_pure_certificates(section):
+def discover_pure_certificates(section):
     for item in section.keys():
         yield Service(item=item)
 
@@ -54,28 +65,33 @@ def check_pure_certificates(item, section):
             summary=f"Item {item} not found",
         )
 
-    data = section[item]
-    txt = f"Common Name: {data['cn']}, Org: {data['org'].replace('_',' ')}, Valid until: {datetime.datetime.fromtimestamp(int(data['valid_to'])/1000).strftime('%Y-%m-%d %H:%M:%S')}"
-    if section[item]['valid'] > 30 :
-        yield Result(
-            state=State.OK,
-            summary=f"OK, {txt}, Days:{data['valid']}",
-        )
-    elif section[item]['valid'] < 15 :
-        yield Result(
-            state=State.CRIT,
-            summary=f"CRIT, Status: {section[item]['status']}, {txt}, Days:{data['valid']}(!!)",
-        )
     else:
-        yield Result(
-            state=State.WARN,
-            summary=f"WARN, {txt}, Days:{data['valid']}",
-        )
+        data = section[item]
+        yield Result(state=State.OK, summary=f"Common Name: {data['cn']}, Org: {data['org'].replace('_',' ')}, Valid until: {datetime.fromtimestamp(int(data['valid_to'])/1000).strftime('%Y-%m-%d %H:%M:%S')}")
+
+        if data['valid'] > 30:
+            yield Result(
+                state=State.OK,
+                summary=f"Days:{data['valid']}",
+            )
+
+        elif data['valid'] < 15:
+            yield Result(
+                state=State.CRIT,
+                summary=f"Status: {data['status']}, Days:{data['valid']}",
+            )
+
+        else:
+            yield Result(
+                state=State.WARN,
+                summary=f"Days:{data['valid']}",
+            )
 
 
-register.check_plugin(
+check_plugin_pure_arraycertificates = CheckPlugin(
     name="pure_arraycertificates",
+    sections=["pure_arraycertificates"],
     service_name="SSL Cert %s",
-    discovery_function=discovery_pure_certificates,
+    discovery_function=discover_pure_certificates,
     check_function=check_pure_certificates,
 )
---
+++ pure/src/pure/agent_based/arrayperformance.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+"""
+Kuhn & Rueß GmbH
+Consulting and Development
+https://kuhn-ruess.de
+"""
+
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    Metric,
+    Result,
+    Service,
+    State,
+)
+from cmk.agent_based.v2.render import (
+    bytes,
+)
+
+
+def parse_pure_arrayperformance(string_table):
+    section = {}
+
+    for row in string_table:
+        item, reads_per_sec, writes_per_sec, output_per_sec, input_per_sec, usec_per_read_op, usec_per_write_op  = row
+
+        section[item] = {
+            'reads_per_sec': reads_per_sec,
+            'writes_per_sec': writes_per_sec,
+            'output_per_sec': output_per_sec,
+            'input_per_sec': input_per_sec,
+            'usec_per_read_op': usec_per_read_op,
+            'usec_per_write_op': usec_per_write_op,        
+        }
+
+    return section
+
+
+agent_section_pure_arrayperformance = AgentSection(
+    name="pure_arrayperformance",
+    parse_function=parse_pure_arrayperformance,
+)
+
+
+def discover_pure_arrayperformance(section):
+    for item in section.keys():
+        yield Service(item=item)
+
+def check_pure_arrayperformance(item, section):
+    failed = []
+
+    if item not in section.keys():
+        yield Result(
+            state=State.UNKNOWN,
+            summary=f"Item {item} not found",
+        )
+
+    else:
+        data=section[item]
+        disk_read_ios=data['reads_per_sec']
+        disk_write_ios=data['writes_per_sec']
+        disk_read_throughput=data['output_per_sec']
+        disk_write_throughput=data['input_per_sec']
+        disk_read_responsetime=data['usec_per_read_op']
+        disk_write_responsetime=data['usec_per_write_op']
+
+        yield Result(state=State.OK, notice=f"Read latency: {data['reads_per_sec']}\n \
+            Write latency: {data['writes_per_sec']}\n \
+            Flash Volume: Yes")
+
+        yield Result(
+            state=State.OK,
+            summary=f"Read: {bytes(data['output_per_sec'])}, Write: {bytes(data['input_per_sec'])}, Read operations: {data['reads_per_sec']}/s, Write operations: {data['writes_per_sec']}/s"
+        )
+        
+        # Metrics
+        yield Metric("disk_read_ios", int(disk_read_ios))
+        yield Metric("disk_write_ios", int(disk_write_ios))
+        yield Metric("disk_read_throughput", int(disk_read_throughput))
+        yield Metric("disk_write_throughput", int(disk_write_throughput))
+        yield Metric("read_latency", int(disk_read_responsetime))
+        yield Metric("write_latency", int(disk_write_responsetime))
+
+
+check_plugin_pure_arrayperformance = CheckPlugin(
+    name="pure_arrayperformance",
+    sections=["pure_arrayperformance"],
+    service_name="Filesystem %s Performance",
+    discovery_function=discover_pure_arrayperformance,
+    check_function=check_pure_arrayperformance,
+)
