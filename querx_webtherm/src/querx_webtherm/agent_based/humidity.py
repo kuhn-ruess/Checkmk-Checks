3472f4c1137819568675bdd13a3229cd94310e4f
Sven Rueß <github@sven-ruess.de>
2025-06-02 12:50:33 +0200

    Querx Webtherm: Fix for Checkmk 2.3
    Signed-off-by: Sven Rueß <github@sven-ruess.de>

--- querx_webtherm/src/querx_webtherm/agent_based/humidity.py
+++ querx_webtherm/src/querx_webtherm/agent_based/humidity.py
@@ -16,26 +16,30 @@ from cmk.agent_based.v2 import (
 from cmk.plugins.lib.humidity import check_humidity
 
 
-def discover_querx_webtherm_humidity(string_table):
-    yield Service()
+def parse_querx_webtherm_humidity(string_table):
+    return float(string_table[0][0])
 
-def check_querx_webtherm_humidity(params, string_table):
-    yield from check_humidity(float(string_table[0][0]), params)
+def discover_querx_webtherm_humidity(section):
+    yield Service(item="Humidity Sensor")
+
+def check_querx_webtherm_humidity(item, params, section):
+    yield from check_humidity(section, params)
 
 
 snmp_section_querx_webtherm_humidity = SimpleSNMPSection(
     name = "querx_webtherm_humidity",
+    detect = contains(".1.3.6.1.2.1.1.1.0", "Querx"),
     fetch = SNMPTree(
-        base = "".1.3.6.1.4.1.3444.1.14.1.2.1.5"",
+        base = ".1.3.6.1.4.1.3444.1.14.1.2.1.5",
         oids = ["2"],
     ),
-    detect = contains(".1.3.6.1.2.1.1.1.0", "Querx"),
+    parse_function = parse_querx_webtherm_humidity,
 )
 
 
 check_plugin_querx_webtherm_humidity = CheckPlugin(
     name = "querx_webtherm_humidity",
-    service_name = "Humidity Sensor",
+    service_name = "%s",
     discovery_function = discover_querx_webtherm_humidity,
     check_function = check_querx_webtherm_humidity,
     check_ruleset_name = "humidity",
--- querx_webtherm/src/querx_webtherm/agent_based/temp.py
+++ querx_webtherm/src/querx_webtherm/agent_based/temp.py
@@ -16,26 +16,30 @@ from cmk.agent_based.v2 import (
 from cmk.plugins.lib.temperature import check_temperature
 
 
-def discover_querx_webtherm_temp(string_table):
-    yield Service()
+def parse_querx_webtherm_temp(string_table):
+    return float(string_table[0][0]) / 10
 
-def check_querx_webtherm_temp(params, string_table):
-    yield from check_temperature(float(info[0][0]) / 10, params, "temperature")
+def discover_querx_webtherm_temp(section):
+    yield Service(item="Temperature Sensor")
+
+def check_querx_webtherm_temp(item, params, section):
+    yield from check_temperature(section, params, "temperature")
 
 
 snmp_section_querx_webtherm_temp = SimpleSNMPSection(
     name = "querx_webtherm_temp",
+    detect = contains(".1.3.6.1.2.1.1.1.0", "Querx"),
     fetch = SNMPTree(
         base = ".1.3.6.1.4.1.3444.1.14.1.2.1.5",
         oids = ["1"],
     ),
-    detect = contains(".1.3.6.1.2.1.1.1.0", "Querx"),
+    parse_function = parse_querx_webtherm_temp,
 )
 
 
 check_plugin_querx_webtherm_temp = CheckPlugin(
     name = "querx_webtherm_temp",
-    service_name = "Temperature Sensor",
+    service_name = "%s",
     discovery_function = discover_querx_webtherm_temp,
     check_function = check_querx_webtherm_temp,
     check_ruleset_name = "temperature",
