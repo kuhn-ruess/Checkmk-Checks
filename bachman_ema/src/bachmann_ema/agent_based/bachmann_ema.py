1da3656269e67105283f77ccaa7c90552d018c37
Bastian Kuhn <mail@bastian-kuhn.de>
2025-06-11 14:16:55 +0200

    Updated Bachmann Ema

--- bachman_ema/src/bachmann_ema/agent_based/bachmann_ema.py
+++ bachman_ema/src/bachmann_ema/agent_based/bachmann_ema.py
@@ -21,9 +21,9 @@ def parse_ema(string_table):
     """
     out = {}
     for line in string_table:
-        in_1, in_2, mode_1, mode_2, switch_1, switch_2 = line
-        out[f'{in_1}/1'] = (mode_1, switch_1)
-        out[f'{in_2}/2'] = (mode_2, switch_2)
+        in_1, in_2, mode_1, mode_2, state_1, state_2, switch_1, switch_2 = line
+        out[f'{in_1}/1'] = (mode_1, switch_1, state_1)
+        out[f'{in_2}/2'] = (mode_2, switch_2, state_2)
     return out
 
 
@@ -36,6 +36,8 @@ snmp_section_bluenet_ema = SimpleSNMPSection(
               "5.1.5", # Second 4 Input/Outputs
               "8.1.4", # BlueNet2GPIOModes for Frist 
               "8.1.5", # BlueNet2GPIOModes for Second
+              "10.1.4", #GPIO State
+              "10.1.5", #GPIO State
               "9.1.4", # BlueNet2GPIOSwitch for First
               "9.1.5", # BlueNet2GPIOSwitch for Second
              ],
@@ -51,22 +53,69 @@ def discover_bluenet_ema(section):
 
 def check_bluenet_ema(item, section):
     state = State.OK
-    mode, switch = section[item]
+    mode, switch, entity_state = section[item]
     gpio_mode = {
-            '0': 'disabled',
-            '2': 'enabled',
-            '6': 's0',
-            '7': 'undefined',
+        '0': 'disabled',
+        '2': 'enabled',
+        '6': 's0',
+        '7': 'undefined',
     }
     yield Result(state=state, summary=f"Mode: {gpio_mode[mode]}")
     gpio_switch = {
-            '0': 'undefined',
-            '3': 'switchable',
-            '5': 'notSwitchable',
-            '1': 'on',
-            '2': 'off',
+        '0': 'undefined',
+        '3': 'switchable',
+        '5': 'notSwitchable',
+        '1': 'on',
+        '2': 'off',
     }
     yield Result(state=state, summary=f"Switch: {gpio_switch[switch]}")
+
+    entity_states = {
+       '0': 'expected',
+       '1': 'undefined',
+       '2': 'ok',
+       '3': 'errorHigh',
+       '4': 'errorLow',
+       '5': 'warningHigh',
+       '6': 'warningLow',
+       '7': 'lost',
+       '8': 'deactivate',
+       '9': 'onAlarmIdentidy',
+       '10': 'offAlarmIdentify',
+       '11': 'onAlarm',
+       '12': 'offAlarm',
+       '13': 'onWarningIdentify',
+       '14': 'offWarningIdentify',
+       '15': 'onWarning',
+       '16': 'offWarning',
+       '17': 'onIdentify',
+       '18': 'offIdentify',
+       '19': 'on',
+       '20': 'off',
+       '21': 'onChildAlarm',
+       '22': 'offChildAlarm',
+       '23': 'onChildWarning',
+       '24': 'offChildWarning',
+       '25': 'childAlarm',
+       '26': 'childWarning',
+       '27': 'lostChild',
+       '36': 'updateInProgress',
+       '37': 'updateError',
+       '38': 'onGoingSwitch',
+       '39': 'high',
+       '40': 'low',
+       '41': 'alarm',
+       '42': 'warning',
+       '43': 'ok',
+       '44': 'disabled',
+       '45': 'fwVersionTooNew',
+    }
+
+    if entity_state == '39':
+        state = State.CRIT
+
+    yield Result(state=state, summary=f"Entity: {entity_states[entity_state]}")
+
 
 check_plugin_bluenet_ema = CheckPlugin(
     name="bluenet_ema",
