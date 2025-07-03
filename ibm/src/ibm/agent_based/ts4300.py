25ffe383b715cc957b6d2d27d3562351a288d74e
Sven Rueß <github@sven-ruess.de>
2025-07-03 15:06:28 +0200

    IBM: Added Tape Lib 4300
    Signed-off-by: Sven Rueß <github@sven-ruess.de>

---
+++ ibm/src/ibm/agent_based/ts4300.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+"""
+Kuhn & Rueß GmbH
+Consulting and Development
+https://kuhn-ruess.de
+"""
+
+from cmk.agent_based.v2 import (
+    Service,
+    Result,
+    State,
+    Metric,
+    SimpleSNMPSection,
+    SNMPTree,
+    matches,
+    CheckPlugin,
+)
+
+def parse_ibm_ts4300(string_table):
+    return string_table
+
+snmp_section_ibm_ts4300 = SimpleSNMPSection(
+    name = "ibm_ts4300",
+    parse_function = parse_ibm_ts4300,
+    fetch = SNMPTree(
+        base = '.1.3.6.1.4.1.14851.3.1.3',
+        oids = [
+            '1.0',
+            '2.0',
+            '3.0',
+            '4.0',
+            '5.0',
+        ],
+    ),
+    detect = matches(".1.3.6.1.4.1.14851.3.1.3.3.0", "IBM"),
+)
+
+def discover_ibm_ts4300(section):
+    yield Service()
+
+def check_ibm_ts4300(section):
+    # [['3573-TL', '3555L3A7800LH9', 'IBM', '1.2.1.0-A00', 'IBM TS4300 Tape Library']
+    model, serial, vendor, version, description = section[0]
+    yield Result(state=State.OK,  summary=f"Model: {model}, Serial: {serial}, Version: {version}, Description: {description}")
+
+check_plugin_ibm_ts4300 = CheckPLugin(
+    name = "ibm_ts4300",
+    service_name = "Library Info",
+    discovery_function = discover_ibm_ts4300,
+    check_function = check_ibm_ts4300,
+)
