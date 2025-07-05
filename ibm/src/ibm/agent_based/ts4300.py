b5768a68a1d08396736b100d9488eb4e61346713
Sven Rueß <github@sven-ruess.de>
2025-07-05 12:55:51 +0200

    IBM: fix issues and imports
    Signed-off-by: Sven Rueß <github@sven-ruess.de>

--- ibm/src/ibm/agent_based/ts4300.py
+++ ibm/src/ibm/agent_based/ts4300.py
@@ -10,8 +10,7 @@ from cmk.agent_based.v2 import (
     Service,
     Result,
     State,
-    Metric,
-    SimpleSNMPSection,
+    SNMPSection,
     SNMPTree,
     matches,
     CheckPlugin,
@@ -20,19 +19,21 @@ from cmk.agent_based.v2 import (
 def parse_ibm_ts4300(string_table):
     return string_table
 
-snmp_section_ibm_ts4300 = SimpleSNMPSection(
+snmp_section_ibm_ts4300 = SNMPSection(
     name = "ibm_ts4300",
     parse_function = parse_ibm_ts4300,
-    fetch = SNMPTree(
-        base = '.1.3.6.1.4.1.14851.3.1.3',
-        oids = [
-            '1.0',
-            '2.0',
-            '3.0',
-            '4.0',
-            '5.0',
-        ],
-    ),
+    fetch = [
+        SNMPTree(
+            base = ".1.3.6.1.4.1.14851.3.1.3",
+            oids = [
+                '1.0',
+                '2.0',
+                '3.0',
+                '4.0',
+                '5.0',
+            ],
+        ),
+    ],
     detect = matches(".1.3.6.1.4.1.14851.3.1.3.3.0", "IBM"),
 )
 
@@ -44,7 +45,7 @@ def check_ibm_ts4300(section):
     model, serial, vendor, version, description = section[0]
     yield Result(state=State.OK,  summary=f"Model: {model}, Serial: {serial}, Version: {version}, Description: {description}")
 
-check_plugin_ibm_ts4300 = CheckPLugin(
+check_plugin_ibm_ts4300 = CheckPlugin(
     name = "ibm_ts4300",
     service_name = "Library Info",
     discovery_function = discover_ibm_ts4300,
