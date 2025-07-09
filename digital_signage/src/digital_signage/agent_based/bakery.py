d0569977bb3fee236f46507019616590ec1c30a0
Bastian Kuhn <mail@bastian-kuhn.de>
2025-07-09 11:39:03 +0200

    Updated Digital Signage Plugin to use newer Checkmk APIs

--- digital_signage/src/digital_signage/agent_based/bakery.py
+++ digital_signage/src/digital_signage/agent_based/bakery.py
@@ -5,9 +5,18 @@ from pathlib import Path
 from cmk.base.cee.plugins.bakery.bakery_api.v1 import register, Plugin, PluginConfig, OS, FileGenerator
 
 def get_files(conf: Any) -> FileGenerator:
+    mode = conf.get('deployment', ("do_not_deploy", 0,0))
+    match mode:
+        case "do_not_deploy", _:
+            return
+        case "cached", float(raw_interval):
+            interval: int | None = int(raw_interval)
+        case "sync", _:
+            interval = None
     yield Plugin(
         base_os=OS.WINDOWS,
         source=Path("digital_signage.ps1"),
+        interval=interval,
     )
 
 register.bakery_plugin(
--- digital_signage/src/digital_signage/rulesets/bakery.py
+++ digital_signage/src/digital_signage/rulesets/bakery.py
@@ -1,27 +1,59 @@
-from cmk.gui.cee.plugins.wato.agent_bakery.rulespecs.utils import RulespecGroupMonitoringAgentsAgentPlugins
-from cmk.gui.plugins.wato import rulespec_registry, HostRulespec
+from cmk.rulesets.v1 import Help, Title
+from cmk.rulesets.v1.form_specs import (
+    CascadingSingleChoice,
+    CascadingSingleChoiceElement,
+    DefaultValue,
+    DictElement,
+    Dictionary,
+    FixedValue,
+    TimeMagnitude,
+    TimeSpan,
+)
+from cmk.rulesets.v1.rule_specs import AgentConfig, Topic
 
-from cmk.gui.valuespec import Alternative, Dictionary, FixedValue, TextAscii, DropdownChoice
 
-
-
-def _valuespec():
-    return Alternative(
-        title = "Digital Signage Monitoring (Windows)",
-        help = "The agent plugin will be deployed",
-        elements=[
-            FixedValue(True,
-                       title="Do deploy plugin",
-                       totext="(enabled)"),
-            FixedValue(None,
-                       title="Do not deploy plugin",
-                       totext="(disabled)"),
-        ],
+def _agent_config_digital_signage() -> Dictionary:
+    return Dictionary(
+        help_text=Help(
+            "This will deploy the agent plug-in <tt>digital_signage</tt>. "
+        ),
+        elements={
+            "deployment": DictElement(
+                required=True,
+                parameter_form=CascadingSingleChoice(
+                    title=Title("Deployment type"),
+                    elements=(
+                        CascadingSingleChoiceElement(
+                            name="sync",
+                            title=Title("Deploy the plug-in and run it synchronously"),
+                            parameter_form=FixedValue(value=None),
+                        ),
+                        CascadingSingleChoiceElement(
+                            name="cached",
+                            title=Title("Deploy the plug-in and run it asynchronously"),
+                            parameter_form=TimeSpan(
+                                displayed_magnitudes=(
+                                    TimeMagnitude.HOUR,
+                                    TimeMagnitude.MINUTE,
+                                )
+                            ),
+                        ),
+                        CascadingSingleChoiceElement(
+                            name="do_not_deploy",
+                            title=Title("Do not deploy the plug-in"),
+                            parameter_form=FixedValue(value=None),
+                        ),
+                    ),
+                    prefill=DefaultValue("sync"),
+                ),
+            ),
+        },
     )
 
-rulespec_registry.register(
-    HostRulespec(
-        group=RulespecGroupMonitoringAgentsAgentPlugins,
-        name="agent_config:digital_signage",
-        valuespec=_valuespec,
-    ))
+
+rule_spec_passwort_age = AgentConfig(
+    name="digital_signage",
+    title=Title("digital_sinage: Digital Signage Monitoring (Windows)"), 
+    topic=Topic.OPERATING_SYSTEM,
+    parameter_form=_agent_config_digital_signage,
+)
