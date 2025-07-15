2e7ba559d9b0204439bd630b9dcf291f24f0ed65
Sven Rueß <github@sven-ruess.de>
2025-07-16 01:33:47 +0200

    Redis service: make it Checkmk 2.4 compatible
    Signed-off-by: Sven Rueß <github@sven-ruess.de>

--- rediscover_service/src/redis_service/rulesets/redis_service.py
+++ rediscover_service/src/redis_service/rulesets/redis_service.py
@@ -1,72 +1,71 @@
 #!/usr/bin/env python3
 
-from cmk.gui.i18n import _
-from cmk.gui.plugins.wato.utils import (
-    notification_parameter_registry,
-    NotificationParameter,
-)
-from cmk.gui.valuespec import (
-    Age,
-    Alternative,
-    CascadingDropdown,
-    DEF_VALUE,
+"""
+Kuhn & Rueß GmbH
+Consulting and Development
+https://kuhn-ruess.de
+"""
+
+from cmk.rulesets.v1 import Help, Title
+from cmk.rulesets.v1.form_specs import (
     Dictionary,
-    DropdownChoice,
-    EmailAddress,
-    FixedValue,
-    HTTPUrl,
-    Integer,
-    IPv4Address,
-    ListChoice,
-    ListOfStrings,
-    Password,
-    TextAreaUnicode,
-    TextInput,
-    Transform,
-    Tuple,
+    DictElement,
+    SingleChoice,
+    SingleChoiceElement,
+    DefaultValue,
+    String,
+)
+from cmk.rulesets.v1.rule_specs import (
+    NotificationParameters,
+    Topic,
 )
 
-@notification_parameter_registry.register
-class NotificationParameterRediscoverService(NotificationParameter):
-    @property
-    def ident(self) -> str:
-        return "rediscover_service.py"
 
-    @property
-    def spec(self):
-        return Dictionary(
-            title=_("Create notification with the following parameters"),
-            help=_("Parameters are used to connect to Checkmk site for using RestAPI."),
-            optional_keys=["url_prefix", "proxy_url", "priority", "sound"],
-            elements=[
-                (
-                    "proto",
-                    DropdownChoice(
-                        title=_("Protocol"),
-                        help=_("Which protocol is used for connecting to Checkmk server"),
-                        choices=[
-                            ("http", _("HTTP")),
-                            ("https", _("HTTPS")),
-                        ]
-                    ),
+def _parameters_redis_service():
+    return Dictionary(
+        title = Title("Create notification with the following parameters"),
+        help_text = Help("Parameters are used to connect to Checkmk site for using RestAPI."),
+        elements = {
+            "proto": DictElement(
+                parameter_form = SingleChoice(
+                    title = Title("Protocol"),
+                    help_text = Help("Which protocol is used for connecting to Checkmk server"),
+                    elements = [
+                        SingleChoiceElement(
+                            name = "http",
+                            title=Title("HTTP")
+                        ),
+                        SingleChoiceElement(
+                            name="https",
+                            title=Title("HTTPS")
+                        ),
+                    ],
+                    prefill = DefaultValue("http"),
                 ),
-                (
-                    "hostname",
-                    TextInput(
-                        title=_("Hostname"),
-                        help=_("Give the Checkmk hostname, where the site is running on"),
-                        size=40,
-                        allow_empty=False,
-                    ),
+                required = True,
+            ),
+            "hostname": DictElement(
+                parameter_form = String(
+                    title = Title("Hostname"),
+                    help_text = Help("Give the Checkmk hostname, where the site is running on"),
+                    field_size = 40,
                 ),
-                (
-                    "sitename",
-                    TextInput(
-                        title=_("Sitename"),
-                        help=_("Enter the name of the Checkmk site"),
-                        size=40,
-                        allow_empty=False,
-                    ),
+                required = True,
+            ),
+            "sitename": DictElement(
+                parameter_form = String(
+                    title = Title("Sitename"),
+                    help_text = Help("Enter the name of the Checkmk site"),
+                    field_size=40,
                 ),
-            ],
-        )
+                required = True,
+            ),
+        },
+    )
+
+rule_spec_redis_service = NotificationParameters(
+    title = Title("Rediscover service"),
+    topic = Topic.OPERATING_SYSTEM,
+    parameter_form = _parameters_redis_service,
+    name = "rediscover_service",
+)
