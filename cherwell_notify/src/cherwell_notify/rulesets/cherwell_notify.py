02a9ffb318702f4d46e2cd625f6a505e466abd21
Sven Rueß <github@sven-ruess.de>
2025-07-16 02:04:39 +0200

    Cherwell: make it Checkmk 2.4 compatible
    Signed-off-by: Sven Rueß <github@sven-ruess.de>

--- cherwell_notify/src/cherwell_notify/rulesets/cherwell_notify.py
+++ cherwell_notify/src/cherwell_notify/rulesets/cherwell_notify.py
@@ -6,93 +6,94 @@ Consulting and Development
 https://kuhn-ruess.de
 """
 
-from cmk.gui.i18n import _
-from cmk.gui.plugins.wato.utils import (
-    notification_parameter_registry,
-    NotificationParameter,
-)
-from cmk.gui.valuespec import (
+from cmk.rulesets.v1 import Help, Title
+from cmk.rulesets.v1.form_specs import (
     Dictionary,
-    TextAscii,
+    DictElement,
+    String,
     Password,
 )
+from cmk.rulesets.v1.form_specs.validators import LengthInRange
+from cmk.rulesets.v1.rule_specs import (
+    NotificationParameters,
+    Topic,
+)
 
-@notification_parameter_registry.register
-class NotificationParameterCherwell(NotificationParameter):
-    @property
-    def ident(self) -> str:
-        return "cherwell_notify.py"
 
+def _parameters_cherwell_notify():
+    return Dictionary(
+        title = Title("Create notification with followingen parameters"),
+        elements = {
+            "api_url": DictElement(
+                parameter_form = String(
+                    title = Title("API URL"),
+                    help_text = Help("Full API URL"),
+                    custom_validate=(LengthInRange(min_value=1),),
+                ),
+                required = True,
+            ),
+            "token_url": DictElement(
+                parameter_form = String(
+                    title = Title("Token API URL"),
+                    help_text = Help("Full Token API URL"),
+                    custom_validate=(LengthInRange(min_value=1),),
+                ),
+                required = True,
+            ),
+            "client_id": DictElement(
+                parameter_form = String(
+                    title = Title("Client ID"),
+                    help_text = Help("Client ID for Authentication"),
+                    custom_validate=(LengthInRange(min_value=1),),
+                ),
+                required = True,
+            ),
+            "username": DictElement(
+                parameter_form = String(
+                    title = Title("Username"),
+                    help_text = Help("Username for Authentication"),
+                    custom_validate=(LengthInRange(min_value=1),),
+                ),
+                required = True,
+            ),
+            "password": DictElement(
+                parameter_form = Password(
+                    title = Title("Auth Password"),
+                    help_text = Help("Password for Authentication"),
+                    custom_validate=(LengthInRange(min_value=1),),
+                ),
+                required = True,
+            ),
+            "automation_secret": DictElement(
+                parameter_form = Password(
+                    title = Title("Automation Secret of Checkmk"),
+                    help_text = Help("Used for the API Call"),
+                    custom_validate=(LengthInRange(min_value=1),),
+                ),
+                required = True,
+            ),
+            "cmk_server": DictElement(
+                parameter_form = String(
+                    title = Title("Checkmk Server"),
+                    help_text = Help("Server Address of Checkmk Server"),
+                    custom_validate=(LengthInRange(min_value=1),),
+                ),
+                required = True,
+            ),
+            "cmk_site": DictElement(
+                parameter_form = String(
+                    title = Title("Checkmk Site"),
+                    help_text = Help("Checkmk Site Name"),
+                    custom_validate=(LengthInRange(min_value=1),),
+                ),
+                required = True,
+            ),
+        },
+    )
 
-    @property
-    def spec(self):
-        return Dictionary(
-            title = _("Create notification with followingen parameters"),
-            optional_keys = [],
-            elements = [
-                (
-                    "api_url",
-                    TextAscii(
-                        title = _("API URL"),
-                        help = _("Full API URL"),
-                        allow_empty = False,
-                    ),
-                ),
-                (
-                    "token_url",
-                    TextAscii(
-                        title = _("Token API URL"),
-                        help = _("Full Token API URL"),
-                        allow_empty = False,
-                    ),
-                ),
-                (
-                    "client_id",
-                    TextAscii(
-                        title = _("Client ID"),
-                        help = _("Client ID for Authentication"),
-                        allow_empty = False,
-                    ),
-                ),
-                (
-                    "username",
-                    TextAscii(
-                        title = _("Username"),
-                        help = _("Username for Authentication"),
-                        allow_empty = False,
-                    ),
-                ),
-                (
-                    "password",
-                    Password(
-                        title = _("Auth Password"),
-                        help = _("Password for Authentication"),
-                        allow_empty = False,
-                    ),
-                ),
-                (
-                    "automation_secret",
-                    Password(
-                        title = _("Automation Secret of Checkmk"),
-                        help = _("Used for the API Call"),
-                        allow_empty = False,
-                    ),
-                ),
-                (
-                    "cmk_server",
-                    TextAscii(
-                        title = _("Checkmk Server"),
-                        help = _("Server Address of Checkmk Server"),
-                        allow_empty = False,
-                    ),
-                ),
-                (
-                    "cmk_site",
-                    TextAscii(
-                        title = _("Checkmk Site"),
-                        help = _("Checkmk Site Name"),
-                        allow_empty = False,
-                    ),
-                ),
-            ],
-        )
+rule_spec_cherwell_notify = NotificationParameters(
+    title = Title("Cherwell notify"),
+    topic = Topic.OPERATING_SYSTEM,
+    parameter_form = _parameters_cherwell_notify,
+    name = "cherwell_notify",
+)
