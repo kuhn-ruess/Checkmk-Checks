8b261e55a7917ed1db0e3b6d9ff205c64c06a9ee
Bastian Kuhn <mail@bastian-kuhn.de>
2025-08-03 20:04:39 +0200

    Added Simple df checks for nfs and cifs

---
+++ nfs_df/src/nfs_df/agent_based/bakery.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+from typing import Any # type: ignore
+from pathlib import Path
+from cmk.base.cee.plugins.bakery.bakery_api.v1 import register, Plugin, PluginConfig, OS, FileGenerator
+
+def get_files(conf: Any) -> FileGenerator:
+    mode = conf.get('deployment', ("do_not_deploy", 0,0))
+    match mode:
+        case "do_not_deploy", _:
+            return
+        case "cached", float(raw_interval):
+            interval: int | None = int(raw_interval)
+        case "sync", _:
+            interval = None
+    yield Plugin(
+        base_os=OS.LINUX,
+        source=Path("nfs_df"),
+        interval=interval,
+    )
+
+register.bakery_plugin(
+    name="nfs_df",
+    files_function=get_files,
+)
---
+++ nfs_df/src/nfs_df/rulesesets/backery.py
@@ -0,0 +1,59 @@
+from cmk.rulesets.v1 import Help, Title
+from cmk.rulesets.v1.form_specs import (
+    CascadingSingleChoice,
+    CascadingSingleChoiceElement,
+    DefaultValue,
+    DictElement,
+    Dictionary,
+    FixedValue,
+    TimeMagnitude,
+    TimeSpan,
+)
+from cmk.rulesets.v1.rule_specs import AgentConfig, Topic
+
+
+def _agent_config_nfs_df() -> Dictionary:
+    return Dictionary(
+        help_text=Help("The plugin <tt>nfs_df</tt> allows "
+                       "monitoring of NFS Mount-Points on Linux Systems"
+        ),
+        elements={
+            "deployment": DictElement(
+                required=True,
+                parameter_form=CascadingSingleChoice(
+                    title=Title("Deployment type"),
+                    elements=(
+                        CascadingSingleChoiceElement(
+                            name="sync",
+                            title=Title("Deploy the plug-in and run it synchronously"),
+                            parameter_form=FixedValue(value=None),
+                        ),
+                        CascadingSingleChoiceElement(
+                            name="cached",
+                            title=Title("Deploy the plug-in and run it asynchronously"),
+                            parameter_form=TimeSpan(
+                                displayed_magnitudes=(
+                                    TimeMagnitude.HOUR,
+                                    TimeMagnitude.MINUTE,
+                                )
+                            ),
+                        ),
+                        CascadingSingleChoiceElement(
+                            name="do_not_deploy",
+                            title=Title("Do not deploy the plug-in"),
+                            parameter_form=FixedValue(value=None),
+                        ),
+                    ),
+                    prefill=DefaultValue("sync"),
+                ),
+            ),
+        },
+    )
+
+
+rule_spec_passwort_age = AgentConfig(
+    name="nfs_df",
+    title=Title("Filesystemmonitoring of NFS Mounts via Plugin (Linux)"), 
+    topic=Topic.OPERATING_SYSTEM,
+    parameter_form=_agent_config_nfs_df,
+)
