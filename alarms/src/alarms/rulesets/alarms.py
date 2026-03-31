dc3c0fd99797efbb352e392153451283fece52b6
Sven Rueß <github@sven-ruess.de>
2026-03-31 16:05:53 +0200

    Updated alarms notifications to Checkmk 2.4
    Signed-off-by: Sven Rueß <github@sven-ruess.de>

--- alarms/src/alarms/rulesets/alarms.py
+++ alarms/src/alarms/rulesets/alarms.py
@@ -6,6 +6,12 @@ Consulting and Development
 https://kuhn-ruess.de
 """
 
+from typing import cast
+
+from cmk.gui.form_specs.vue.visitors.recomposers.unknown_form_spec import recompose
+from cmk.gui.valuespec import Dictionary as ValueSpecDictionary
+from cmk.gui.watolib.notification_parameter import notification_parameter_registry, NotificationParameter
+
 from cmk.rulesets.v1 import Help, Title
 from cmk.rulesets.v1.form_specs import (
     Dictionary,
@@ -15,89 +21,90 @@ from cmk.rulesets.v1.form_specs import (
     DefaultValue,
     String,
 )
-from cmk.rulesets.v1.rule_specs import (
-    NotificationParameters,
-    Topic,
-)
 
 
-def _parameters_alarms():
-    return Dictionary(
-        title = Title("Create notification with the following parameters"),
-        help_text = Help("Parameters are used to connect to API to play alarms."),
-        elements = {
-            "proto": DictElement(
-                parameter_form = SingleChoice(
-                    title = Title("Protocol"),
-                    help_text = Help("Which protocol is used for connecting to API"),
-                    elements = [
-                        SingleChoiceElement(
-                            name = "http",
-                            title=Title("HTTP")
-                        ),
-                        SingleChoiceElement(
-                            name="https",
-                            title=Title("HTTPS")
-                        ),
-                    ],
-                    prefill = DefaultValue("http"),
-                ),
-                required = True,
-            ),
-            "hostname": DictElement(
-                parameter_form = String(
-                    title = Title("Hostname"),
-                    help_text = Help("Give the hostname, where the API is running on"),
-                    field_size = 40,
-                    prefill = DefaultValue("localhost"),
-                ),
-                required = True,
-            ),
-            "url": DictElement(
-                parameter_form = String(
-                    title = Title("URL"),
-                    help_text = Help("Enter the URI of the API"),
-                    field_size=40,
-                    prefill = DefaultValue("api.php"),
-                ),
-                required = True,
-            ),
-            "alarm": DictElement(
-                parameter_form = SingleChoice(
-                    title = Title("Alarm"),
-                    help_text = Help("Which alarm should be played"),
-                    elements = [
-                        SingleChoiceElement(
-                            name = "alarm1",
-                            title=Title("Alarm 1")
-                        ),
-                        SingleChoiceElement(
-                            name="alarm2",
-                            title=Title("Alarm 2")
-                        ),
-                        SingleChoiceElement(
-                            name="alarm3",
-                            title=Title("Alarm 3")
-                        ),
-                        SingleChoiceElement(
-                            name="alarm4",
-                            title=Title("Alarm 4")
-                        ),
-                        SingleChoiceElement(
-                            name="horse",
-                            title=Title("Horse")
-                        ),
-                    ],
-                    prefill = DefaultValue("alarm1"),
-                ),
-                required = True,
-            ),
-        },
-    )
+class NotificationParameterAlarms(NotificationParameter):
+    @property
+    def ident(self) -> str:
+        return "alarms"
 
-rule_spec_alarms = NotificationParameters(
-    title = Title("Play alarms"),
-    topic = Topic.OPERATING_SYSTEM,
-    parameter_form = _parameters_alarms,
-    name = "alarms",
-)
+    @property
+    def spec(self) -> ValueSpecDictionary:
+        return cast(ValueSpecDictionary, recompose(self._form_spec()).valuespec)
+
+    def _form_spec(self) -> Dictionary:
+        return Dictionary(
+            title = Title("Play alarms (using API)"),
+            help_text = Help("Parameters are used to connect to API to play alarms."),
+            elements = {
+                "proto": DictElement(
+                    parameter_form = SingleChoice(
+                        title = Title("Protocol"),
+                        help_text = Help("Which protocol is used for connecting to API"),
+                        elements = [
+                            SingleChoiceElement(
+                                name = "http",
+                                title=Title("HTTP")
+                            ),
+                            SingleChoiceElement(
+                                name="https",
+                                title=Title("HTTPS")
+                            ),
+                        ],
+                        prefill = DefaultValue("http"),
+                    ),
+                    required = True,
+                ),
+                "hostname": DictElement(
+                    parameter_form = String(
+                        title = Title("Hostname"),
+                        help_text = Help("Give the hostname, where the API is running on"),
+                        field_size = 40,
+                        prefill = DefaultValue("localhost"),
+                    ),
+                    required = True,
+                ),
+                "url": DictElement(
+                    parameter_form = String(
+                        title = Title("URL"),
+                        help_text = Help("Enter the URI of the API"),
+                        field_size=40,
+                        prefill = DefaultValue("api.php"),
+                    ),
+                    required = True,
+                ),
+                "alarm": DictElement(
+                    parameter_form = SingleChoice(
+                        title = Title("Alarm"),
+                        help_text = Help("Which alarm should be played"),
+                        elements = [
+                            SingleChoiceElement(
+                                name = "alarm1",
+                                title=Title("Alarm 1")
+                            ),
+                            SingleChoiceElement(
+                                name="alarm2",
+                                title=Title("Alarm 2")
+                            ),
+                            SingleChoiceElement(
+                                name="alarm3",
+                                title=Title("Alarm 3")
+                            ),
+                            SingleChoiceElement(
+                                name="alarm4",
+                                title=Title("Alarm 4")
+                            ),
+                            SingleChoiceElement(
+                                name="horse",
+                                title=Title("Horse")
+                            ),
+                        ],
+                        prefill = DefaultValue("alarm1"),
+                    ),
+                    required = True,
+                ),
+            },
+        )
+
+
+notification_parameter_registry.register(NotificationParameterAlarms)
