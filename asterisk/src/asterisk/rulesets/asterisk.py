812c5be0de0f1ba82d42c0702eb73b43541ebe6e
Bastian Kuhn <mail@bastian-kuhn.de>
2026-06-18 20:21:02 +0200

    asterisk 2.1.0: port notification ruleset to rulesets.v1 for Checkmk 2.5

--- asterisk/src/asterisk/rulesets/asterisk.py
+++ asterisk/src/asterisk/rulesets/asterisk.py
@@ -6,110 +6,111 @@ Consulting and Development
 https://kuhn-ruess.de
 """
 
-from cmk.gui.i18n import _
-from cmk.gui.plugins.wato.utils import (
-    NotificationParameter,
-    IndividualOrStoredPassword,
-    notification_parameter_registry,
-)
-from cmk.gui.valuespec import (
+from cmk.rulesets.v1 import Help, Title
+from cmk.rulesets.v1.form_specs import (
+    DictElement,
     Dictionary,
-    TextInput,
+    String,
+    Password,
     Integer,
+    DefaultValue,
+)
+from cmk.rulesets.v1.form_specs.validators import LengthInRange
+from cmk.rulesets.v1.rule_specs import (
+    NotificationParameters,
+    Topic,
 )
 
 
-@notification_parameter_registry.register
-class NotificationParamterAsterisk(NotificationParameter):
-    @property
-    def ident(self) -> str:
-        return "asterisk"
+def _parameters_asterisk():
+    return Dictionary(
+        title=Title("Create notification with the following parameters"),
+        elements={
+            "host": DictElement(
+                parameter_form=String(
+                    title=Title("Asterisk Server IP"),
+                    help_text=Help("Please specify Asterisk server IP."),
+                    custom_validate=(LengthInRange(min_value=1),),
+                ),
+                required=True,
+            ),
+            "port": DictElement(
+                parameter_form=Integer(
+                    title=Title("Port of AMI API"),
+                    help_text=Help("Please specify port of AMI API."),
+                    prefill=DefaultValue(5038),
+                ),
+                required=True,
+            ),
+            "timeout": DictElement(
+                parameter_form=Integer(
+                    title=Title("Timeout for calling"),
+                    help_text=Help("Please specify timeout for call. It must be higher than call time"),
+                    prefill=DefaultValue(180),
+                ),
+                required=True,
+            ),
+            "username": DictElement(
+                parameter_form=String(
+                    title=Title("Username"),
+                    help_text=Help("The user, used for login, has to have at least the call, command and originate"),
+                    custom_validate=(LengthInRange(min_value=1),),
+                ),
+                required=True,
+            ),
+            "password": DictElement(
+                parameter_form=Password(
+                    title=Title("Password of the user"),
+                ),
+                required=True,
+            ),
+            "channel": DictElement(
+                parameter_form=String(
+                    title=Title("Channel used for calling"),
+                    help_text=Help("Please specify the channel, which should be used for calling"),
+                    custom_validate=(LengthInRange(min_value=1),),
+                ),
+                required=True,
+            ),
+            "exten": DictElement(
+                parameter_form=String(
+                    title=Title("Extension used for calling"),
+                    help_text=Help("Please specify th exten, which should be used for calling"),
+                    custom_validate=(LengthInRange(min_value=1),),
+                ),
+                required=True,
+            ),
+            "priority": DictElement(
+                parameter_form=Integer(
+                    title=Title("Priority used for calling"),
+                    help_text=Help("Please specify the priority, which should be used for calling"),
+                    prefill=DefaultValue(1),
+                ),
+                required=True,
+            ),
+            "context": DictElement(
+                parameter_form=String(
+                    title=Title("Context used for calling"),
+                    help_text=Help("Please specify the contect, which should be used for calling"),
+                    custom_validate=(LengthInRange(min_value=1),),
+                ),
+                required=True,
+            ),
+            "callerid": DictElement(
+                parameter_form=String(
+                    title=Title("Caller ID used for calling"),
+                    help_text=Help("Please specify the caller ID, which should be used for calling"),
+                    custom_validate=(LengthInRange(min_value=1),),
+                ),
+                required=True,
+            ),
+        },
+    )
 
-    @property
-    def spec(self) -> Dictionary:
-        return Dictionary(
-            title=_("Create notification with the following parameters"),
-            optional_keys=[],
-            elements=[
-                (
-                    "host",
-                    TextInput(
-                        title=_("Asterisk Server IP"),
-                        help=_("Please specify Asterisk server IP."),
-                        allow_empty=False,
-                    ),
-                ),
-                (
-                    "port",
-                    Integer(
-                        title=_("Port of AMI API"),
-                        help=_("Please specify port of AMI API."),
-                        default_value=5038,
-                    ),
-                ),
-                (
-                    "timeout",
-                    Integer(
-                        title=_("Timeout for calling"),
-                        help=_("Please specify timeout for call. It must be higher than call time"),
-                        default_value=180,
-                    ),
-                ),
-                (
-                    "username",
-                    TextInput(
-                        title=_("Username"),
-                        help=_("The user, used for login, has to have at least the call, command and originate"),
-                        allow_empty=False,
-                    ),
-                ),
-                (
-                    "password",
-                    IndividualOrStoredPassword(
-                        title=_("Password of the user"),
-                        allow_empty=False,
-                    ),
-                ),
-                (
-                    "channel",
-                    TextInput(
-                        title=_("Channel used for calling"),
-                        help=_("Please specify the channel, which should be used for calling"),
-                        allow_empty=False,
-                    ),
-                ),
-                (
-                    "exten",
-                    TextInput(
-                        title=_("Extension used for calling"),
-                        help=_("Please specify th exten, which should be used for calling"),
-                        allow_empty=False,
-                    ),
-                ),
-                (
-                    "priority",
-                    Integer(
-                        title=_("Priority used for calling"),
-                        help=_("Please specify the priority, which should be used for calling"),
-                        default_value=1,
-                    ),
-                ),
-                (
-                    "context",
-                    TextInput(
-                        title=_("Context used for calling"),
-                        help=_("Please specify the contect, which should be used for calling"),
-                        allow_empty=False,
-                    ),
-                ),
-                (
-                    "callerid",
-                    TextInput(
-                        title=_("Caller ID used for calling"),
-                        help=_("Please specify the caller ID, which should be used for calling"),
-                        allow_empty=False,
-                    ),
-                ),
 
-            ],
-        )
+rule_spec_asterisk = NotificationParameters(
+    title=Title("Asterisk notification"),
+    topic=Topic.NOTIFICATIONS,
+    parameter_form=_parameters_asterisk,
+    name="asterisk",
+)
