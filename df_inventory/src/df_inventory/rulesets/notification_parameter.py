e3eadb244c80f0a50778dfe61c88d36314010bfe
Bastian Kuhn <mail@bastian-kuhn.de>
2026-06-18 20:21:01 +0200

    df_inventory 2.1.0: port notification ruleset to rulesets.v1 for Checkmk 2.5

--- df_inventory/src/df_inventory/rulesets/notification_parameter.py
+++ df_inventory/src/df_inventory/rulesets/notification_parameter.py
@@ -1,34 +1,13 @@
-from cmk.gui.watolib.notification_parameter import notification_parameter_registry, NotificationParameter
+from cmk.rulesets.v1 import Title
+from cmk.rulesets.v1.rule_specs import NotificationParameters, Topic
 
 #@ TODO BK: Risky Path, check on CMK Updates
-from cmk.gui.wato._notification_parameter import _mail as mail
-
-from typing import cast
-from cmk.gui.valuespec import Dictionary as ValueSpecDictionary
-from cmk.gui.form_specs.vue.visitors.recomposers.unknown_form_spec import recompose
-
-# This May work after 2.4
-#class NotificationParameterDfMail(NotificationParameter):
-#    """
-#    Notification parameter for DF Mail
-#    """
-#    @property
-#    def ident(self) -> str:
-#        return "df_mail"
-#
-#    @property
-#    def spec(self) -> ValueSpecDictionary:
-#        # TODO needed because of mixed Form Spec and old style setup
-#        return cast(ValueSpecDictionary, recompose(self._form_spec()).valuespec)
-#
-#    def _form_spec(self):
-#        return mail.form_spec_mail
-
-class NotificationParameterDfMail(mail.NotificationParameterMail):
-
-    @property
-    def ident(self) -> str:
-        return "df_mail"
+from cmk.gui.wato._notification_parameter._mail import form_spec_mail
 
 
-notification_parameter_registry.register(NotificationParameterDfMail)
+rule_spec_df_mail = NotificationParameters(
+    name="df_mail",
+    title=Title("Filesystem DF Mail"),
+    topic=Topic.NOTIFICATIONS,
+    parameter_form=form_spec_mail,
+)
